855317ab622fa246182ddd7ed15eb50b28493dd1
Venkateswara BOJJA <venkateswara.bojja_ext@michelin.com>
2018-09-09 20:07:01 +0200

    changed java file for testing jenkins

--- src/main/java/org/bojja/mvnapp/App.java
+++ src/main/java/org/bojja/mvnapp/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+		System.out.println("Executing the code from github");
     }
 	public String printMessage(String message)
 	{
